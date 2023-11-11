ebfd7bc3d2e42a7ae077fea624fb49e8c5dc0c39
Joshua Hecker <jhecker2001@gmail.com>
2023-11-11 10:00:55 -0600

    Altered program so it receives questions from a trivia API.

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 from question_model import Question
-from data import question_data
 from quiz_brain import QuizBrain
+import requests
+
+parameters = {
+    "amount": 10,
+    "type": "boolean"
+}
+
+response = requests.get(url="https://opentdb.com/api.php", params=parameters)
+response.raise_for_status()
+question_list = response.json()["results"]
+print(question_list)
 
 question_bank = []
-for question in question_data:
+for question in question_list:
     question_text = question["question"]
     question_answer = question["correct_answer"]
     new_question = Question(question_text, question_answer)
