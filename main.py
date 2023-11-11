8fb2af542db53a65b916023de8374bbca3125ded
Joshua Hecker <jhecker2001@gmail.com>
2023-11-11 10:36:53 -0600

    Created UI.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from question_model import Question
 from quiz_brain import QuizBrain
+from ui import QuizInterface
 import requests
 
 parameters = {
@@ -20,9 +21,10 @@ for question in question_data:
 
 
 quiz = QuizBrain(question_bank)
+quiz_ui = QuizInterface()
 
-while quiz.still_has_questions():
-    quiz.next_question()
+# while quiz.still_has_questions():
+#     quiz.next_question()
 
 print("You've completed the quiz")
 print(f"Your final score was: {quiz.score}/{quiz.question_number}")
